46ad11a4e19aa48d828be79a461a302909edd5b6
mysticis <eshokhlah@gmail.com>
2020-03-20 15:21:30 +0100

    implement sorting of blogs

--- src/App.js
+++ src/App.js
@@ -97,13 +97,15 @@ const App = () => {
 
   //List of blogs to view
   const blogsList = blogs => {
-    return blogs.map(blog => (
-      <Blog
-        key={blog.id}
-        blog={blog}
-        updateLikes={() => updateLikes(blog.id)}
-      />
-    ))
+    return blogs
+      .sort((a, b) => b.likes - a.likes)
+      .map(blog => (
+        <Blog
+          key={blog.id}
+          blog={blog}
+          updateLikes={() => updateLikes(blog.id)}
+        />
+      ))
   }
 
   //Logout function
@@ -171,4 +173,14 @@ const updateLikes = async id => {
           setMessage(null)
         }, 5000)
       })
+  }*/
+/*
+ const blogsList = blogs => {
+    return blogs.map(blog => (
+      <Blog
+        key={blog.id}
+        blog={blog}
+        updateLikes={() => updateLikes(blog.id)}
+      />
+    ))
   }*/
